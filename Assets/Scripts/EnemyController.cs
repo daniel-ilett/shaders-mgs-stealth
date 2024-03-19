7f1cba4f1e5f6ccebc1d19bf4b2cb882ed052587
Daniel Ilett <danielilett@gmail.com>
2024-03-19 16:51:27 +0000

    Added exclamation point + other changes.

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
+    [SerializeField] private Transform exclaimationMark;
+
     private Animator animator;
 
     private void Start()
@@ -16,6 +18,34 @@ public class EnemyController : MonoBehaviour
         if(Input.GetMouseButtonDown(0))
         {
             animator.SetTrigger("Point");
+            StartCoroutine(ExclaimAnimation());
         }
+    }
+
+    private IEnumerator ExclaimAnimation()
+    {
+        Vector3 originalScale = exclaimationMark.localScale;
+        exclaimationMark.gameObject.SetActive(true);
+
+        Vector3 startScale = new Vector3(originalScale.x * 4.0f, 0.0f, 1.0f);
+        Vector3 endScale = originalScale;
+        float animTime = 0.1f;
+
+        for (float t = 0.0f; t < animTime; t += Time.deltaTime)
+        {
+            exclaimationMark.localScale = Vector3.Lerp(startScale, endScale, t / animTime);
+            yield return null;
+        }
+
+        yield return new WaitForSeconds(0.8f);
+
+        for (float t = 0.0f; t < animTime; t += Time.deltaTime)
+        {
+            exclaimationMark.localScale = Vector3.Lerp(endScale, startScale, t / animTime);
+            yield return null;
+        }
+
+        exclaimationMark.localScale = originalScale;
+        exclaimationMark.gameObject.SetActive(false);
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,6 +6,10 @@ public class PlayerController : MonoBehaviour
 {
     private Animator animator;
 
+    [SerializeField] private List<Renderer> renderers;
+    [SerializeField] private Material stealthMaterial;
+    [SerializeField] private GameObject stealthItemIcon;
+
     private void Start()
     {
         animator = GetComponent<Animator>();
@@ -15,5 +19,22 @@ public class PlayerController : MonoBehaviour
     {
         bool isRunning = (Input.GetKey(KeyCode.Space));
         animator.SetBool("IsRunning", isRunning);
+
+        if(Input.GetKeyDown(KeyCode.S))
+        {
+            for(int i = 0; i < renderers.Count; ++i)
+            {
+                var materials = renderers[i].materials;
+
+                for(int j = 0; j < materials.Length; ++j)
+                {
+                    materials[j] = stealthMaterial;
+                }
+
+                renderers[i].materials = materials;
+            }
+
+            stealthItemIcon.SetActive(true);
+        }
     }
 }
